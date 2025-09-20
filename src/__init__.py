aa3475c35aaa70b7730900877fb59c6cbd479570
finn-nnif <finnsullivank@gmail.com>
2025-09-20 18:38:44 +0800

    Util Package Support

--- src/__init__.py
+++ src/__init__.py
@@ -8,10 +8,12 @@ from .data_loader import load_csv_all, load_csv_range
 from .data_cleaner import clean_data, remove_char
 from .data_transformer import pivot_data, round_up_to_nearest
 from .data_visualisation import plot_histogram, plot_scatter, plot_line, plot_multi_line
-from .data_design import style_plot, style_title, style_line_annotation, design_lines, design_points, expand_limits
+from .data_design import style_plot, style_title, style_line_annotation, design_points, expand_limits
 
+# util
+from .utility.util_graph import *
 # config
-from config.config import COLOUR_PALETTE
+from config.config import *
 
 __all__ = [
     'pd', 'plt', 'yaml',
@@ -20,6 +22,7 @@ __all__ = [
     'pivot_data', 'round_up_to_nearest',
     'plot_histogram', 'plot_scatter', 'plot_line', 'plot_multi_line',
     'style_plot', 'style_title', 'style_line_annotation',
-    'design_lines', 'design_points', 'expand_limits',
+    'design_points', 'expand_limits',
+    'move_legend_outside',
     'COLOUR_PALETTE'
 ]
